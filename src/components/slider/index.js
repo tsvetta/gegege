252309cf805a4dc654bfdc0057f9db446c0f6fc7
Tatyana Tsvtekova <tsvettacsssr@yandex-team.ru>
2018-03-04 18:11:15 +0300

    add custom scrollbar

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 import styles from './styles.css';
 const cx = classnames.bind(styles);
@@ -11,18 +12,37 @@ class Slider extends React.PureComponent {
 
     return (
       <div className={styles.wrapper}>
-        <ul className={cx('slider', {
-          [`theme_${props.theme}`]: Boolean(props.theme),
-        })}>
-          {React.Children.map(props.children, child => (
-            <li className={styles.slide}>
-              {React.cloneElement(child, {
-                className: cx(child.props.className, styles.slideContent),
-              })}
-            </li>
-          ))}
-        </ul>
+        <Scrollbars
+          autoHide
+          autoHideTimeout={200}
+          renderTrackHorizontal={this.renderTrack}
+          renderThumbHorizontal={this.renderThumb}
+        >
+          <ul className={cx('slider', {
+            [`theme_${props.theme}`]: Boolean(props.theme),
+          })}>
+            {React.Children.map(props.children, child => (
+              <li className={styles.slide}>
+                {React.cloneElement(child, {
+                  className: cx(child.props.className, styles.slideContent),
+                })}
+              </li>
+            ))}
+          </ul>
+        </Scrollbars>
       </div>
+    );
+  }
+
+  renderTrack(props) {
+    return (
+      <div {...props} className={styles.track} />
+    );
+  }
+
+  renderThumb(props) {
+    return (
+      <div {...props} className={styles.thumb} />
     );
   }
 }
